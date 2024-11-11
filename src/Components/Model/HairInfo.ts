5fb970a4db3e7bbe3df97a97485a10446a2e6dda
Chanmin Lee <lee00837@algonquinlive.com>
2024-11-11 14:50:30 -0700

    added price page

--- src/Components/Model/HairInfo.ts
+++ src/Components/Model/HairInfo.ts
@@ -15,16 +15,14 @@ export type Address = {
     zipCode: string;
 }
 
-type ServiceCategory = 'Hair Cut' | 'Hair Perm' | 'Coloring' | 'Treatment' | 'Hair Extension' | 'Hair Styling' | 'Hair Spa';
+type ServiceCategory = 'Cut' | 'Perm' | 'Color' | 'Styling/Care';
 
 type HairCutCategory = 'Men Hair Cut' | 'Women Hair Cut';
 type HairPermCategory = 'Men Hair Perm' | 'Men Heat Perm' | 'Men Magic Straight Perm' | 'Women Hair Perm' | 'Women Heat Perm' | 'Women Magic Straight Perm';
 type HairColorCategory = 'Men\'s Coloring' | 'Women\'s Coloring' | 'Color Refresh' | 'Ombre' | 'Base Touch Up';
-type HairTreatmentCategory = 'Men Hair Treatment' | 'Women Hair Treatment';
-type HairStylingCategory = 'Shampoo & Blowout' | 'Curl' | 'Straightening' | 'Special Occasion' | 'Bridal' | 'Updo';
-type HairSpaCategory = 'Scalp Massage';
+type HairStylingCategory = 'Shampoo & Blowout' | 'Men Hair Treatment' | 'Women Hair Treatment' | 'Straightening' | 'Special Occasion' | 'Bridal' | 'Updo' | 'Hair Extension' | 'Scalp Massage';
 
-type SubCategory = HairCutCategory | HairPermCategory | HairColorCategory | HairTreatmentCategory | 'Hair Extension' | HairStylingCategory | HairSpaCategory;
+type SubCategory = HairCutCategory | HairPermCategory | HairColorCategory | HairStylingCategory;
 
 
 export type Service = {
